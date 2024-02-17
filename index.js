9b39733a5666d08502ade3a3229805c68a4ffe16
Vidar Eldøy <vidar@eldoy.com>
2024-02-17 02:33:26 +0100

    Move functions to lib

--- index.js
+++ index.js
@@ -1,55 +1,11 @@
 var path = require('path')
 var lodash = require('lodash')
-var { addHook } = require('pirates')
+
+var { lazyload, byFileName } = require('./lib/util.js')
 
 var { tree, basext, env } = require('extras')
 
 var LAZYLOADABLE = ['js', 'json', 'mjs', 'cjs', 'wasm', 'node']
-
-function lazy(val, path, mode, fn, props) {
-  if (typeof val != 'undefined') {
-    return val
-  }
-
-  function hook(content, name) {
-    if (typeof fn == 'function') {
-      content = fn({ ...props, content })
-    }
-    return content
-  }
-
-  var removeHook = addHook(hook, { exts: ['.js'] })
-  var content = require(path)
-  removeHook()
-
-  return content
-}
-
-function lazyload(path, mode, fn, props) {
-  var val
-  var handler = {
-    get: (target, key) => {
-      val = lazy(val, path, mode, fn, props)
-      return Reflect.get(val, key)
-    },
-    apply: (target, self, args) => {
-      val = lazy(val, path, mode, fn, props)
-      return Reflect.apply(val, self, args)
-    },
-    construct: (target, args) => {
-      val = lazy(val, path, mode, fn, props)
-      return Reflect.construct(val, args)
-    }
-  }
-  return new Proxy(function () {}, handler)
-}
-
-// Sort by number in file name
-function byFileName(a, b) {
-  var [b1, x1] = basext(a)
-  var [b2, x2] = basext(b)
-  return (b1.match(/^\d+/g) || b1) - (b2.match(/^\d+/g) || b2)
-}
 
 function load(dir, opt, fn) {
   if (typeof opt == 'function') {
@@ -95,8 +51,6 @@ function load(dir, opt, fn) {
 }
 
 function loadAsync(dir, opt, fn) {
-  var promises = []
-
   return new Promise(function (r) {
     r(load(dir, opt, fn))
   })
